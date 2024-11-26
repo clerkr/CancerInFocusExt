16b647167ace80fc8973c72f4adbadfef7067fcd
dstone42 <david.gstone42@gmail.com>
2024-11-26 12:20:28 -0700

    Added icon, updated one logo, setup dev env

--- ShinyCIF/deploy/cif.sh
+++ ShinyCIF/deploy/cif.sh
@@ -1,8 +1,21 @@
 
-docker build -t cif-ext -f ShinyCIF/deploy/Dockerfile .
+#### Prod Env ####
+
+# docker build -t cif-ext -f ShinyCIF/deploy/Dockerfile .
+
+# docker run \
+#     -v /srv/external:/srv/external \
+#     -v /var/log/shiny-server:/var/log/shiny-server \
+#     -p 3838:3838 \
+#     cif-ext
+
+#### Dev Env ###
+
+docker build --platform linux/x86_64 -t cif-ext -f ShinyCIF/deploy/individual.Dockerfile .
 
 docker run \
-    -v /srv/external:/srv/external \
+    -v /Users/davidstone/Projects/SHAPE/external:/srv/external \
     -v /var/log/shiny-server:/var/log/shiny-server \
     -p 3838:3838 \
+    --name cif-ext \
     cif-ext
--- ShinyCIFBivar/deploy/bivar.sh
+++ ShinyCIFBivar/deploy/bivar.sh
@@ -1,8 +1,21 @@
 
-docker build -t individual_bivar-ext -f ShinyCIFBivar/deploy/individual.Dockerfile .
+#### Prod Env ####
+
+# docker build -t bivar-ext -f ShinyCIFBivar/deploy/Dockerfile .
+
+# docker run \
+#     -v /srv/external/:/srv/external/ \
+#     -v /var/log/shiny-server:/var/log/shiny-server \
+#     -p 3840:3840 \
+#     bivar-ext
+
+#### Dev Env ####
+
+docker build --platform linux/x86_64 -t bivar-ext -f ShinyCIFBivar/deploy/individual.Dockerfile .
 
 docker run \
     -v /srv/external/:/srv/external/ \
     -v /var/log/shiny-server:/var/log/shiny-server \
     -p 3840:3840 \
-    individual_bivar-ext
+    --name bivar-ext \
+    bivar-ext
