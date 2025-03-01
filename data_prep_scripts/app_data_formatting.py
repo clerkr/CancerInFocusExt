5aedd911d482a4ee2ece4340013b8050fe110dce
clerkr <j.clark.roylance@gmail.com>
2025-02-28 23:47:47 -0700

    Created a script for preparing tract data for use in the app map presentation

---
+++ data_prep_scripts/app_data_formatting.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+fl_comp = "/Users/jonathanroylance/Downloads/facility_load_comprehensive.csv"
+all_tracts = '/Users/jonathanroylance/CS_Projects/BIO465/CancerInFocus/CancerInFocusExt/ShinyCIF/www/data/all_tract.csv'
+
+df = pd.read_csv(fl_comp)
+df['CountyTract'] = df['CountyTract'].astype(str).str[1:]
+df['CountyTract'] = df['CountyTract'].astype(int)
+df.rename(columns={'CountyTract' : 'GEOID'}, inplace=True)
+
+df_t = pd.read_csv(all_tracts)
+df_t = df_t[['GEOID', 'Tract', 'County']].drop_duplicates()
+
+df_t = df_t.groupby(['GEOID', 'County']).agg({'Tract': lambda x: min(x, key=len)}).reset_index()
+
+df = df.merge(df_t, on='GEOID', how='left')
+df.drop(columns=['miles'], inplace=True)
+df.rename(columns={'minutes' : 'value'}, inplace=True)
+
+df['lbl'] = df['value'].round().astype(int).astype(str)
+df['measure'] = 'Minutes'
+df['fmt'] = 'float64'
+
+new_order =['GEOID', 'Tract', 'County', 'State', 'FacID', 'FacType', 'measure', 'value', 'fmt', 'lbl']
+df = df[new_order]
+
+df.to_csv('/Users/jonathanroylance/Downloads/tract_min_FORAPP.csv', index=False)
