0995f83a67de0747d91a03f89dafb621424c6114
mads66 <carter36611@gmail.com>
2025-03-12 14:13:15 -0600

    Pre-categorical data changes with functional Sufficient Broadband coverage maps

--- ShinyCIF-Clark/www/data/Switching_cat_and_def_columns.R
+++ ShinyCIF-Clark/www/data/Switching_cat_and_def_columns.R
@@ -6,6 +6,8 @@ my_data <- read_csv(file.path(getwd(), "tract_min_FORAPP.csv"))
 
 broadband_data <- read_csv(file.path(getwd(), "shape_broadband_tract.csv"))
 
+sufficiency_data <- read_csv(file.path(getwd(), "broadband_sufficiency.csv"))
+
 #FIX GASTROENTEROLOGY TO COLONOSCOPY
 #
 # Swap values of 'cat' and 'def' columns
@@ -25,9 +27,11 @@ broadband_clean <- broadband_data %>%
   mutate(cat = "Broadband Coverage") %>% #changing cat for our app
   mutate(lbl = as.character(lbl)) %>%  # Convert 'lbl' to character
   mutate(FacID = NA) #adding in NA for facility ID
-  
 
-merged_data <- bind_rows(new_data, broadband_clean)
+sufficiency_data <- sufficiency_data %>%
+  mutate(lbl = as.character(lbl))
+
+merged_data <- bind_rows(new_data, broadband_clean, sufficiency_data)
 
 # Save to a new CSV file
 write_csv(merged_data, "all_tract_capstone_complete.csv")
